a18c6b698501102b15b71c466073197523b6a6ec
l0la960 <oyinlolaolaniyan47@gmail.com>
2025-07-28 12:21:58 +0100

    updated

--- assignment.js
+++ assignment.js
@@ -10,7 +10,20 @@ let adviceBank = [
   "The only journey is the one within.",
    "Where 1there is hatred, let me sow love.",
   " Live as if you were to die tomorrow. Learn as if you were to live forever.",
-  "We accept the love we think we deserve."
+  "We accept the love we think we deserve." ,
+  "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart." ,
+ "Believe in yourself. You are braver than you think, more talented than you know, and capable of more than you imagine." ,
+ "It's not the load that breaks you down, it's the way you carry it." ,
+ "Do not fear failure but rather fear not trying." ,
+ "You cannot change anyone, but you can be the reason someone changes" ,
+ "Life becomes easier and more beautiful when we can see the good in other people." ,
+ "It is better to be hated for what you are than to be loved for what you are not." , 
+ "Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin." ,
+ "You can have it all. Just not all at once." ,
+ "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+ "Try not to become a man of success. Rather become a man of value." ,
+ "Sometimes it takes a good fall to really know where you stand" , 
+ "It is hard to fail, but it is worse never to have tried to succeed."
 ]
 
 dice.addEventListener("click", () => {
